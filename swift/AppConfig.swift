bbebe22c7a288432d79e9b2fc3b6253552b849f3
Doach Sync <dev@doach.local>
2025-10-23 12:49:44 -0700

    swift update

--- swift/AppConfig.swift
+++ swift/AppConfig.swift
@@ -3,6 +3,6 @@ import Foundation
 enum AppConfig {
     static let shotAnalysisURL = URL(string: "https://www.doach.app")!
     static let apiBaseURL = URL(string: "https://api.doach.app")!
-    static let registrationPath = "auth/register"
+    static let registrationPath = "api/auth/register"
     static let loginURL = URL(string: "https://www.doach.app/login")!
 }
